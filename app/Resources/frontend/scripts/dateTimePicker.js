e0600eb4d6ba44f34cf45ccdf13b1be60a41e4f8
LubinAshley <ashleyl@heavy-drive.com>
2017-11-08 22:31:54 +0100

    [styles] Update style and js

---
+++ app/Resources/frontend/scripts/dateTimePicker.js
@@ -0,0 +1,6 @@
+
+    jQuery(function () {
+        jQuery('#js-datepicker1').datetimepicker({ format:'DD-MM-YYYY HH:mm:ss' });
+        jQuery('#js-datepicker2').datetimepicker({ format:'DD-MM-YYYY HH:mm:ss' });
+
+    });
---
+++ app/Resources/frontend/scripts/front.js
@@ -0,0 +1,5 @@
+jQuery(function () {
+    jQuery('#js-datepicker1').datetimepicker({ format:'DD-MM-YYYY HH:mm:ss' });
+    jQuery('#js-datepicker2').datetimepicker({ format:'DD-MM-YYYY HH:mm:ss' });
+
+});
